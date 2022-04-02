3160e8ec9f53fdb20fc3d3f41698b348c6fa4ed1
moruno21 <amoruno21@gmail.com>
2022-04-02 14:18:59 +0200

    feat: head component moved to layout file

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,8 @@
 import { useSession } from 'next-auth/react';
 import React from 'react';
+import Head from 'next/head';
 import Navbar from './Navbar';
+import styles from '../styles/Home.module.css';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -13,8 +15,13 @@ const Layout = ({ children }: LayoutProps) => {
 
   return (
     <>
+      <Head>
+        <title>Booked</title>
+      </Head>
       {session ? <Navbar /> : <></>}
-      <main>{children}</main>
+      <div className={styles.container}>
+        <main>{children}</main>
+      </div>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,16 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import { Session } from 'next-auth';
 import { getSession, useSession } from 'next-auth/react';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import BookList from '../components/BookList';
 import LogIn from './login';
 
 const Home = ({ session }: { session: Session }) => {
   if (!session) return <LogIn />;
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Booked</title>
-      </Head>
-      <BookList></BookList>
-    </div>
-  );
+  return <BookList></BookList>;
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
-  // Instead of redirecting, we check within the component whether there's a session or not (so we return the login component)
-  // if (!session)
-  //   return {
-  //     redirect: {
-  //       destination: '/login',
-  //       permanent: false,
-  //     },
-  //   };
-
   return {
     props: {
       session: session,
