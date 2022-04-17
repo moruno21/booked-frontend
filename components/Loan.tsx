da2ef1cd9441fa03228ded1f8696531acdeb251c
moruno21 <amoruno21@gmail.com>
2022-04-17 23:35:03 +0200

    feat: book name shown in loans

--- components/Loan.tsx
+++ components/Loan.tsx
@@ -3,6 +3,9 @@ import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import ILoan from '../types/loan';
 import { Button } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import IBook from '../types/book';
 
 const StyledTableCell = styled(TableCell)(() => ({
   fontSize: 14,
@@ -19,13 +22,30 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 const Loan = ({ loan }: { loan: ILoan }) => {
+  const { data: session } = useSession();
+  const [loanBook, setLoanBook] = useState<IBook>();
+
   const initialDate = new Date(loan.initialDate);
   const finalDate = new Date(loan.finalDate);
+
+  useEffect(() => {
+    const getBook = async () => {
+      const res = await fetch(`http://localhost:3500/books/${loan.bookId}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${session?.accessToken}`,
+        },
+      });
+      const data = await res.json();
+      setLoanBook(data);
+    };
+    getBook();
+  }, []);
 
   return (
     <StyledTableRow key={loan._id}>
       <StyledTableCell component="th" scope="loan">
-        {loan.bookId}
+        {loanBook?.title}
       </StyledTableCell>
       <StyledTableCell>{`${initialDate.getDate()}/${initialDate.getMonth()}/${initialDate.getFullYear()}`}</StyledTableCell>
       <StyledTableCell>
