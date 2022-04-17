72fae3a2cb14fba3714561fa2e554fe9ca6b6acd
moruno21 <amoruno21@gmail.com>
2022-04-17 15:34:38 +0200

    useless index removed

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -26,7 +26,7 @@ const BookList = () => {
   return (
     <Box sx={{ flexGrow: 1, m: 8 }}>
       <Grid container spacing={{ xs: 1, sm: 2, md: 3 }} columns={{ xs: 1, sm: 8, md: 12 }}>
-        {books?.map((book: IBook, index) => (
+        {books?.map((book: IBook) => (
           <Book key={book._id} book={book} />
         ))}
       </Grid>
